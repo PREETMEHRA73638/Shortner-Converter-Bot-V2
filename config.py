a3f4a782320c0f6188fc8a3adf05fb1ba551e1bf
PREETMEHRA73638 <109457025+PREETMEHRA73638@users.noreply.github.com>
2023-12-05 23:20:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://G:g@cluster0.cbchwas.mongodb.net/?retryWrites=true&w=majority") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "6651109872")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+ADMINS.append(6651109872)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001707609134")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "MOVIES_VILLA_UPDATE") # For Force Subscription
